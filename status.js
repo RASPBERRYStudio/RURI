d39123ba96178d6b578a83743866fdc5e8a2b3c0
sanaehururu1200 <sanaehururu1200@gmail.com>
2018-11-07 00:29:31 +0900

    記憶xハジマリ

---
+++ status.js
@@ -0,0 +1,53 @@
+const moment = require("moment");
+const store = require('nedb');
+
+/* 
+now,nextEffect
+0 : 忘れ,特になし
+1~1000: 良い情報
+-1~-1000: 悪い情報
+
+impression
+words: 記憶に残るような言葉
+number: 記憶に残るような会話のナンバー、["1000-1002"]のように記述
+*/
+
+const memoryBase = JSON.parse(`{
+  "now": {
+    "emotion": {
+      "joy": 0,
+      "anger": 0,
+      "sadness": 0,
+      "happiness": 0
+    },
+    "think": {
+      "yearn": 0,
+      "love": 0
+    }
+  },
+  "nextEffect": {
+    "emotion": {
+      "joy": 0,
+      "anger": 0,
+      "sadness": 0,
+      "happiness": 0
+    },
+    "think": {
+      "yearn": 0,
+      "love": 0
+    }
+  },
+  "impression": {
+    "words": [],
+    "number": []
+  },
+  "created_at": "${now = moment().unix()}"
+}`);
+
+var db = new store({
+  filename: 'memory/memory.db',
+  autoload: true
+});
+
+console.log(baseJson);
+
